be4b8a77f012e4f1e193f0a857278852be52f204
SamueleIntaschi <s.intaschi2@studenti.unipi.it>
2021-05-10 16:04:03 +0200

    Added comments

--- BLE_client_ESP32/BLE_client_ESP32.ino
+++ BLE_client_ESP32/BLE_client_ESP32.ino
@@ -44,7 +44,7 @@ String actual_stop = "";
 static boolean doConnect = false;
 static boolean doAnalysis = false;
 
-// Returns the index of the first free position in the servers array
+// Returns the index of the first free position in the array that stores the servers
 int getFirstFreeServerIndex() {
   for (int i=0; i<servers_number; i++) {
     if (servers[i].device == NULL) return i;
@@ -53,7 +53,7 @@ int getFirstFreeServerIndex() {
 }
 
 
-// Resetting device information
+// Resets device information
 void resetServer(server* s) {
   s->device = NULL;
   s->pRemoteCharacteristic = NULL;
@@ -65,7 +65,7 @@ void resetServer(server* s) {
 }
 
 
-// Connect to servers
+// Connects to servers
 bool connectToServers() {
   for (int i=0; i<servers_number; i++) {
     if (servers[i].device != NULL && servers[i].conn == false) {
@@ -82,8 +82,8 @@ bool connectToServers() {
         return false;
       }
   
-      // Connect to the remove BLE Server.
-      res = servers[i].pClient->connect(servers[i].device);  // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
+      // Connect to the remote BLE Server.
+      res = servers[i].pClient->connect(servers[i].device);
       if (res == 1) {
         Serial.println(" - Connected to server");
       }
@@ -102,7 +102,8 @@ bool connectToServers() {
         resetServer(&servers[i]);
         return false;
       }
-      // Obtain a reference to the characteristic in the service of the remote BLE server.
+
+      // Obtain references to the characteristics in the service of the remote BLE server.
       servers[i].pRemoteCharacteristic = servers[i].pRemoteService->getCharacteristic(charUUID);
       if (servers[i].pRemoteCharacteristic == nullptr) {
         Serial.print("Failed to find our characteristic UUID: ");
@@ -119,7 +120,9 @@ bool connectToServers() {
         resetServer(&servers[i]);
         return false;
       }
+
       servers[i].conn = true;
+      // Update number of connected servers
       connected_number++;
     }
   }
@@ -131,22 +134,24 @@ int checkIfAlreadyPresent(BLEAdvertisedDevice* device) {
   int i = 0;
   for (i=0; i<servers_number; i++) {
     if (servers[i].device != NULL) {
+      // If this server is found return its index
       if (servers[i].device->getAddress().equals(device->getAddress())) return i;
     }
   }
+  // Return -1 if the server is not already present
   return -1;
 }
 
-// Scan for the servers that offer the desired service or if we are at a bus stop
+// Scan for the servers that offer the desired service or for detecting bus stops
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
   // Called for each advertised device
   void onResult(BLEAdvertisedDevice advertisedDevice) {
     Serial.print("BLE Advertised Device found: ");
     Serial.println(advertisedDevice.toString().c_str());
-    // We have found a device, let us now see if it contains the service we are looking for.
+    // Found a device, see if it contains the service we are looking for.
     if (advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(serviceUUID)) {
       Serial.println("Found a server");
-      //If the server offers this service and it is not already present, adds it to the servers array
+      // If the server offers this service and it is not already present, adds it to the servers array
       BLEAdvertisedDevice* device = new BLEAdvertisedDevice(advertisedDevice);
       int serverIndex = checkIfAlreadyPresent(device);
       if (serverIndex == -1) {
@@ -155,7 +160,7 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
           // Saving this server
           servers[actual_server].device = device;
           servers[actual_server].conn = false;
-          //-100 is the value that represent no data
+          //-100 is the value that represents no data
           servers[actual_server].value = -100;
           doConnect = true;
         }
@@ -169,25 +174,19 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
       std::string strServiceData = advertisedDevice.getServiceData();
       uint8_t cServiceData[100];
       strServiceData.copy((char *)cServiceData, strServiceData.length(), 0);
-      //Serial.printf("Eddystone: %d %s length %d\n", advertisedDevice.getServiceDataUUID().bitSize(), advertisedDevice.getServiceDataUUID().toString().c_str(),strServiceData.length());
-      //Get Service Data
+      // Get Service Data
       boolean found = true;
       int j = 0;
+      // Check if the namespace id is what we are looking for
       for (int i=2;i<12;i++) {
         if (cServiceData[i] != stopService[j]) found = false;
         j++;
       }
       if (found == true) {
-        //Serial.println("Found a stop: " + advertisedDevice->getAddress().toString().c_str());
-        String id = "";
-        // Instance id to know the identifier of the bus stop
+        // Get instance id to know the identifier of the bus stop
         String instanceId = "";
-        for (int i=2;i<12;i++) {
-          int number = cServiceData[i];
-          String n = String(number);
-          id = id + n;
-        }
         for (int i=12;i<strServiceData.length();i++) {
+          // Convert from hex to int
           int number = (int) cServiceData[i];
           String n = String(number);
           instanceId = instanceId + n;
@@ -200,8 +199,8 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
           // Starting the video analysis
           doAnalysis = true;
         }
-      }//Found bus stop 
-    } //Found eddystone beacon
+      } //Found bus stop 
+    } //Found an eddystone beacon
   } // onResult
 }; // MyAdvertisedDeviceCallbacks
 
@@ -225,6 +224,7 @@ void reconnect() {
   }
 }
 
+// Setup procedure
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting ESP32 application...");
@@ -254,9 +254,8 @@ void setup() {
   Serial.println ("Connected");
   setClock ();
 
-  // MQTT
+  // Set MQTT server
   client.setServer(mqtt_server, 1883);
-  //This is for received messages, not this case
 } // End of setup.
 
 // Set time via NTP, as required for x.509 validation
@@ -277,11 +276,12 @@ void setClock () {
   Serial.print (asctime (&timeinfo));
 }
 
-// Publish the delta for the last stop over MQTT
+// Publishes the delta for the last stop over MQTT
 void publishCount(const char* count) {
   Serial.print("Delta people: ");
   Serial.println(count);
   String deltastr = String(count);
+  // Transform the values in a JSON string and publish it
   String json = "{\"d\":" + deltastr + ", \"s\":" + actual_stop + "}";
   client.publish("line1/bus1", json.c_str());
   int i = 0;
@@ -296,18 +296,17 @@ void publishCount(const char* count) {
   values_number = 0;
 }
 
-// This is the Arduino main loop function.
+// This is the ESP-32 main loop function.
 void loop() {
 
   // If the flag "doConnect" is true then we have scanned for and found the desired
-  // BLE Server with which we wish to connect.  Now we connect to it.  Once we are 
-  // connected we set the connected flag to be true.
+  // BLE servers we wish to connect with.
   if (doConnect == true) {
     connectToServers();
     doConnect = false;
   }
   
-  // Communicates to the servers that we are at a stop
+  // Communicates to the servers that we are at a stop if doAnalysis is true
   if (doAnalysis == true) {
     for (int i=0; i<servers_number; i++) {
       if (servers[i].device != NULL && servers[i].conn == true) {
@@ -325,7 +324,7 @@ void loop() {
   }
   client.loop();
 
-  // Compute delta and publish when all servers have communicated it
+  // Compute delta and publish the info when all servers have communicated it
   for (int i=0; i<servers_number; i++) {
     if (servers[i].device != NULL && servers[i].conn == true) {
       // Read the value of the characteristic.
@@ -334,32 +333,35 @@ void loop() {
         const char* val = value.c_str();
         int strLength = strlen(val);
         if (strLength > 0) {
+          // Convert value string to int
           int v = atoi(val);
-          // Checks if the delta is not '#', else the doors are closed
+          // Checks if the delta is not '#', else the value is ignored
           if (val[0] != '#') {
             Serial.print("The characteristic value is: ");
             Serial.println(val);
             servers[i].value = v;
+            // Increment the number of useful values received
             values_number++;
+            // Compute the delta until now
             delta = delta + v;
           }
         }
         else {
-          //Device disconnection if the characteristic is empty
+          // Device disconnection if the characteristic is empty
           resetServer(&servers[i]);
           connected_number--;
         }
       }
-      //When all the connected servers have communicated the value, publish the total sum 
+      // When all the connected servers have communicated the value, publish the total sum 
       if ((values_number > 0) && (values_number == connected_number)) {
-        //publish
+        // publish
         char deltastr[8];
         itoa(delta, deltastr, 10);
         publishCount(deltastr);
       }
     }
   }
-  //Rescan to find new devices for 1 second
+  // Rescan to find new devices for 1 second
   BLEDevice::getScan()->start(1);
   delay(1000); // Delay 1 seconds between loops.
 } // End of loop
